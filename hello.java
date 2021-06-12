8ac8b05d9ed8bc72f8f1c0897e64da501f575ff2
rakibahasan <bd.rakibahasan@gmail.com>
2021-06-12 22:53:50 +0600

    Added saySomething method

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ public class Hello{
     public static void main(String[] ){
         System.out.println("Vs code is Awesome...");
         greeting("Rakib Ahasan");
+        saySomething("how are you?");
     }
     public void greeting(String name){
         System.out.println("Welcome" + name);
+    }
+    public void saySomething(String s){
+        System.out.println("Hello dear...");
     }
 }
