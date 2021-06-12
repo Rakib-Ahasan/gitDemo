ed64e4794f5d8b71d39d55a205db971714a62aa8
rakibahasan <bd.rakibahasan@gmail.com>
2021-06-12 22:42:08 +0600

    Added greeting method

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[] ){
         System.out.println("Vs code is Awesome...");
+        greeting("Rakib Ahasan");
+    }
+    public void greeting(String name){
+        System.out.println("Welcome" + name);
     }
 }
