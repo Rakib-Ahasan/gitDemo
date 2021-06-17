3d4af7da6690385a7a76bf17870a2c4e7a50469d
rakibahasan <bd.rakibahasan@gmail.com>
2021-06-17 20:05:52 +0600

    change method called area

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class Hello{
         greeting("Rakib Ahasan");
         message();
     }
-    public static void sqr(int a){
-        return a*a;
+    public static void area(int a,int b, int c){
+        return a*b*c;
     }
 }
